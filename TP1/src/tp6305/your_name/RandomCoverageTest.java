0fdddd964e36e862375f58d872f60974a8e3f4a4
Mehdi Kadi <mehdi.kadi@polymtl.ca>
2019-01-16 15:22:11 -0500

    add random

--- TP1/src/tp6305/your_name/RandomCoverageTest.java
+++ TP1/src/tp6305/your_name/RandomCoverageTest.java
@@ -13,18 +13,23 @@ public class RandomCoverageTest extends CoverageTest {
 
 	protected void randomData(StringBuilder builder, float[] testData) {
 		
-		TODO: 
-		
-		
+		//TODO: 
 		//Generate random numbers to each elements in testData, 
 		//then put them in the StringBuilder as:		
 		//builder.append(testData[i]).append(", ");
+	
+	   for (int i = 0; i < testData.length; i++) 
+        { 
+        	Random rand = new Random();
+        	int randomNumber = rand.nextInt(50) + 1; // entre [1,50]
+        	builder.append(randomNumber).append(", ");
+        }
 			
 	}
 	
 	protected double computeBranchCoverage(List<String> instrumentingOutputs, String testData) {
 
-		TODO: 
+		// TODO: 
 		// Compute it accumulatively.
 		// Only system.out.println(testData) when they contribute to increase the coverage.
 
