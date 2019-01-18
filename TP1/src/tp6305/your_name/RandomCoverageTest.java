f872dd97f335d15230ac43c8bdd58bd7df3392e7
bilal itani <bilal.itani1234@gmail.com>
2019-01-18 18:35:28 -0500

    Added RandomCoverageTest

--- TP1/src/tp6305/your_name/RandomCoverageTest.java
+++ TP1/src/tp6305/your_name/RandomCoverageTest.java
@@ -1,44 +1,75 @@
 package tp6305.your_name;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Random;
 
 import tp6305.CoverageTest;
 
 public class RandomCoverageTest extends CoverageTest {
+
+	private static final int MIN = -10;
+	private static final int MAX = 10;
+	private Random rand;
+	private double coverage = 0.0;
+	
+	public RandomCoverageTest()
+	{
+		rand = new Random();
+	}
 	
 	protected void generateTestData(StringBuilder builder, float[] testData) {
 		randomData(builder, testData);
 	}
 
 	protected void randomData(StringBuilder builder, float[] testData) {
-		
-		//TODO: 
-		//Generate random numbers to each elements in testData, 
-		//then put them in the StringBuilder as:		
-		//builder.append(testData[i]).append(", ");
-	
-	   for (int i = 0; i < testData.length; i++) 
-        { 
-        	Random rand = new Random();
-        	int randomNumber = rand.nextInt(50) + 1; // entre [1,50]
-        	builder.append(randomNumber).append(", ");
-        }
-			
+
+		// TODO:
+		// Generate random numbers to each elements in testData,
+		// then put them in the StringBuilder as:
+		// builder.append(testData[i]).append(", ");
+		for (int i = 0; i < testData.length; i++) {
+			float randomNumber = MIN + rand.nextFloat() * (MAX - MIN);
+			randomNumber = round(randomNumber, 2);
+			testData[i] = randomNumber;
+			builder.append(randomNumber + ", ");
+		}
+
 	}
 	
+	private float round(float d, int decimalPlace) {
+	    BigDecimal bd = new BigDecimal(Float.toString(d));
+	    bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+	    return bd.floatValue();
+	}
+
 	protected double computeBranchCoverage(List<String> instrumentingOutputs, String testData) {
 
-		// TODO: 
+		// TODO:
 		// Compute it accumulatively.
-		// Only system.out.println(testData) when they contribute to increase the coverage.
-
-		return 0.0;
+		// Only system.out.println(testData) when they contribute to increase the
+		// coverage.
+		/*this.branchesTested.size() */
+		/*coverage =*/
+		for(String s : instrumentingOutputs)
+		{
+			if(s.contains("trace"))
+			{
+				this.branchesTested.add(s);
+			}
+		}
+		double currCoverage = ((double) this.branchesTested.size() / (double)this.TOTAL_BRANCH_NUM);
+		if( currCoverage > coverage)
+		{
+			coverage = currCoverage;
+			System.out.println(testData);
+		}
+		return coverage;
 	}
 
 	@Override
 	protected double computeRACC(List<String> outputs, String testData) {
 		return 0;
 	}
-	
+
 }
