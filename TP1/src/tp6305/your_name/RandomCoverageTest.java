796be7ab9330be9ea2cc273e023c4c97f84c3f88
Mehdi Kadi <mehdi.kadi@polymtl.ca>
2019-01-26 15:50:13 -0500

    add comments

--- TP1/src/tp6305/your_name/RandomCoverageTest.java
+++ TP1/src/tp6305/your_name/RandomCoverageTest.java
@@ -10,45 +10,47 @@ public class RandomCoverageTest extends CoverageTest {
 
 	private static final int MIN = -10;
 	private static final int MAX = 10;
-	private Random rand;
 	private double coverage = 0.0;
 	
 	public RandomCoverageTest()
 	{
-		rand = new Random();
 	}
 	
 	protected void generateTestData(StringBuilder builder, float[] testData) {
 		randomData(builder, testData);
 	}
 
-	protected void randomData(StringBuilder builder, float[] testData) {
-
-		// TODO:
-		// Generate random numbers to each elements in testData,
-		// then put them in the StringBuilder as:
-		// builder.append(testData[i]).append(", ");
-		for (int i = 0; i < testData.length; i++) {
+	// TODO : Generate random numbers to each elements in testData
+	protected void randomData(StringBuilder builder, float[] testData) 
+	{	
+		for (int i = 0; i < testData.length; i++) 
+		{
+			// Initialization of the random seed 
+			Random rand = new Random();
+			// Generate a random float number between -10 and 10
 			float randomNumber = MIN + rand.nextFloat() * (MAX - MIN);
+			// Round the generated number to 2 decimals 
+			// used to increase the probability for getting more similar numbers 
 			randomNumber = round(randomNumber, 2);
+			// Insert the generated number in the testData
 			testData[i] = randomNumber;
+			// Put the generated number in the StringBuilder
 			builder.append(randomNumber + ", ");
 		}
-
 	}
 	
-	private float round(float d, int decimalPlace) {
+	private float round(float d, int decimalPlace) 
+	{
+		// function used to truncate a float number to 2 decimals.
 	    BigDecimal bd = new BigDecimal(Float.toString(d));
 	    bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
 	    return bd.floatValue();
 	}
 
-	protected double computeBranchCoverage(List<String> instrumentingOutputs, String testData) {
-
-		// TODO:
-		// Compute it accumulatively.
-		// Only system.out.println(testData) when they contribute to increase the
-		// coverage.
+	// TODO: Compute branch coverage accumulatively.
+	protected double computeBranchCoverage(List<String> instrumentingOutputs, String testData) 
+	{
+		// For each instrumented output check if it contains trace then add it to the set
 		for(String s : instrumentingOutputs)
 		{
 			if(s.contains("trace"))
@@ -56,7 +58,9 @@ public class RandomCoverageTest extends CoverageTest {
 				this.branchesTested.add(s);
 			}
 		}
-		double currCoverage = ((double) this.branchesTested.size() / (double)this.TOTAL_BRANCH_NUM);
+		// Calculate current coverage accumulatively
+		double currCoverage = ((double) this.branchesTested.size() / (double)TOTAL_BRANCH_NUM);
+		// Only system.out.println(testData) when they contribute to increase the coverage
 		if( currCoverage > coverage)
 		{
 			coverage = currCoverage;
