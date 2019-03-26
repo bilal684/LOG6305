6b9d175b6b7d062ca621e826b18825d90d047e9e
Bilal <bilal.itani1234@gmail.com>
2019-03-26 10:04:06 -0400

    added basic transformations.

--- TP4/transformer.py
+++ TP4/transformer.py
@@ -1,49 +1,65 @@
 import numpy as np
 from skimage.measure import compare_ssim as ssim
+from skimage import transform
+from skimage import exposure
+from skimage import filters
+from skimage import restoration
+from skimage import util
 
 def rotate(img, rad_angle):
     # to complete
-    pass
+    #pass
+    return transform.AffineTransform(img, rotation=rad_angle) #transform.rotate(img, math.degrees(rad_angle))
 
 def translate(img, trans_x, trans_y):
     # to complete
-    pass
+    #pass
+    return transform.AffineTransform(img, translation=(trans_x, trans_y))
 
 def scale(img, scale_1, scale_2):
     # to complete 
-    pass
+    #pass
+    return transform.AffineTransform(img, scale=(scale_1, scale_2))
 
 def shear(img, factor):
     # to complete 
-    pass
+    #pass
+    return transform.AffineTransform(img, shear=factor)
     
 def blur(img, sigma):
     # to complete
-    pass
+    #pass
+    return filters.gaussian(img, sigma=sigma)
 
 def change_brightness(img, factor):
     # to complete
-    pass
+    #pass
+    return exposure.adjust_gamma(img, gain=factor)
 
 def change_contrast(img, factor):
     # to complete 
-    pass
+    #pass
+    return exposure.adjust_gamma(img, gamma=factor)
 
 def change_sharpness(img, factor):
     # to complete
-    pass
+    #pass
+    return restoration.denoise_bilateral(img, sigma_spatial=factor)
 
 def add_random_noise(img, delta, proba):
     # to complete
-    pass
+    #pass
+    return util.random_noise(img, mean=delta, amount=proba)
 
 def build_random_transformation():
     # to complete
-    pass
+    #pass
+    return
 
-def compute_similarity(first_img, second_ing):
+def compute_similarity(first_img, second_img):
     # to complete
-    pass 
+    #pass
+    return ssim(first_img, second_img)
 
 def apply_transformation(image_origin, transformation):
     image = np.copy(image_origin)
